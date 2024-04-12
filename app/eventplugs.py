9499fe5f308b7f9a20a21e36dd8463cfba0ffee1
Dodzi Keglo <stephanekeglo@gmail.com>
2024-04-12 01:17:59 +0000

    Bind bootstrap-flask extension to flask app instance

--- app/eventplugs.py
+++ app/eventplugs.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from extensions import db, migrate
+from extensions import db, migrate, bootstrap
 
 
 def create_app():
@@ -10,19 +10,27 @@ def create_app():
     # Initialize Flask extensions
     db.init_app(app)
     migrate.init_app(app, db)
+    bootstrap.init_app(app)
+
 
     # Flask application context
     with app.app_context():
         # Import models
         from auth.models import Users
+        from publish.models import Publishers, Categories
+        from suscribe.models import Suscriptions, Topics, Contents, Events
 
         db.create_all()
 
     # Import blueprints
     from auth import auth_bp
+    from publish import publish_bp
+    from suscribe import suscribe_pb
 
     # Register blueprints
     app.register_blueprint(auth_bp)
+    app.register_blueprint(publish_bp, url_prefix='/publisher')
+    app.register_blueprint(suscribe_pb, url_prefix='/suscription')
 
     return app
 
